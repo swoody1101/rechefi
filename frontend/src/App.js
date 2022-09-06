fc15d10dd8e699405be6f3f1d3bd7faf1a519e5a
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2022-09-06 16:44:58 +0900

    feat: add recipe item(incomplete), add example image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import { Global } from "@emotion/react";
 import { Reset } from "./common/styles/global";
+import RecipeListItem from "./pages/Recipe/components/recipe_list_item";
+
+// DEBUG
 
 function App() {
   return (
     <div className="App">
       <Global styles={Reset} />
+      <RecipeListItem></RecipeListItem>
     </div>
   );
 }
---
+++ frontend/src/pages/Recipe/styles/recipe_list_item_style.js
@@ -0,0 +1,7 @@
+import { css } from "@emotion/react";
+
+export const CardImageStyle = css`
+  max-height: 100%;
+  width: "auto";
+  padding: 100px;
+`;
