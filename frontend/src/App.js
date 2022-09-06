f24257cb0753a32f3f8b842a10d1152c04cdfb03
정진 <supermilktank8@gmail.com>
2022-09-06 23:40:47 +0900

    feat: 메인화면 UI 작업

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import { Global } from "@emotion/react";
 import { Reset } from "./common/styles/global";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainPage from "./pages/Main/main_page";
+import Layout from "./common/components/layout";
+import Test from "./pages/test";
 
 function App() {
   return (
     <div className="App">
       <Global styles={Reset} />
+      <Layout>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/recipe" element={<Test />} />
+            <Route path="/account" element={<MainPage />} />
+          </Routes>
+        </BrowserRouter>
+      </Layout>
     </div>
   );
 }
---
+++ frontend/src/pages/test.jsx
@@ -0,0 +1,5 @@
+const Test = () => {
+  return <div>적용이 잘 되나요</div>;
+};
+
+export default Test;
