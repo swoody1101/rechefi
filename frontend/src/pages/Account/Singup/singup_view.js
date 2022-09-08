643b08480c1eb11acb3f53b6e66a8c83e9025ba0
swoody <swoody1101@gmail.com>
2022-09-08 15:21:05 +0900

    feat: create input_elements.js

---
+++ frontend/src/pages/Account/Singup/singup_view.js
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+
+import {
+  createTheme,
+  Box,
+  Button,
+  Container,
+  FormControl,
+  Grid,
+  ThemeProvider,
+} from "@mui/material";
+
+import InputElement from "../components/input_element";
+
+const SignUp = () => {
+  const theme = createTheme();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
+  const [nickname, setNickname] = useState("");
+
+  const emailCheck = () => {
+    if (!email) {
+      alert("이메일을 확인해주세요.");
+      return false;
+    }
+    return true;
+  };
+
+  const passwordCheck = () => {
+    if (!password) {
+      alert("비밀번호를 확인해주세요.");
+      return false;
+    }
+    if (password !== rePassword) {
+      alert("비밀번호가 일치하지 않습니다.");
+      return false;
+    }
+    return true;
+  };
+
+  const nicknameCheck = () => {
+    if (!nickname) {
+      alert("닉네임을 입력해주세요.");
+      return false;
+    }
+    return true;
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (!emailCheck() || !passwordCheck() || !nicknameCheck()) {
+      return;
+    }
+
+    const userObj = {
+      email: email,
+      password: password,
+      nickname: nickname,
+    };
+    window.localStorage.setItem("userName", JSON.stringify(userObj));
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="sm">
+        <Box
+          component="form"
+          noValidate
+          onSubmit={formSubmitHandler}
+          sx={{ mt: 10 }}
+        >
+          <FormControl component="fieldset" variant="standard">
+            <Grid container spacing={2}>
+              <InputElement
+                label="이메일"
+                type="text"
+                value={email}
+                setValue={setEmail}
+                defaultText="이메일을 입력해주세요."
+              />
+              <InputElement
+                label="비밀번호"
+                type="password"
+                value={password}
+                setValue={setPassword}
+                defaultText="비밀번호를 입력해주세요."
+              />
+              <InputElement
+                label="비밀번호 확인"
+                type="password"
+                value={rePassword}
+                setValue={setRePassword}
+                defaultText="비밀번호를 한번 더 입력해주세요."
+              />
+              <InputElement
+                label="닉네임"
+                type="text"
+                value={nickname}
+                setValue={setNickname}
+                defaultText="닉네임을 입력해주세요."
+              />
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="outlined"
+              size="large"
+              sx={{ mt: 10 }}
+            >
+              회원가입
+            </Button>
+          </FormControl>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+};
+
+export default SignUp;
