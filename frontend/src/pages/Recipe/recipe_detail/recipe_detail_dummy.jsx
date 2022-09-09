4d25b9b5e84f2bf60ebd7a42aec1ee74348f73be
정진 <supermilktank8@gmail.com>
2022-09-09 14:12:39 +0900

    feat: 댓글, 대댓글 기능 추가

--- frontend/src/pages/Recipe/recipe_detail/recipe_detail_dummy.jsx
+++ frontend/src/pages/Recipe/recipe_detail/recipe_detail_dummy.jsx
@@ -3,7 +3,7 @@ export const dummyDetail = {
   member_nickname: "안녕하세요",
   title: "중학생도 만들 수 있는 쉬운",
   content:
-    "```<p>먼저 근처 짜장면집에 전화를 걸어서 탕수육 大자를 시킵니다.</p>```<img></img>```<p>배달이 오면 맛있게 먹습니다.</p>",
+    "<p>먼저 근처 짜장면집에 전화를 걸어서 탕수육 大자를 시킵니다.</p>```<img src='https://media.discordapp.net/attachments/433506654009425921/979195859524788244/1014134412025bc65e924b01d5ffd1a3578c7262.gif'></img>```<p>배달이 오면 맛있게 먹습니다.</p>```<img style='width: 100%;' src='https://cdn.discordapp.com/attachments/433506654009425921/986506798636884029/unknown.png'></img>",
   ingredients: [
     {
       name: "양파",
@@ -40,3 +40,33 @@ export const dummyDetail = {
   like_member_id: [1],
   comments: [],
 };
+
+export const dummyDetailComment = [
+  {
+    member_id: 1,
+    member_nickname: "기타치는이현태",
+    content: "이거 보고 제 애완 돼지가 춤을 추고 있습니다",
+    create_at: "2022-09-07",
+    root: 0,
+    group: 1,
+    sequence: 1,
+  },
+  {
+    member_id: 2,
+    member_nickname: "기타안치는이현태",
+    content: "와! 애완 돼지!",
+    create_at: "2022-09-07",
+    root: 0,
+    group: 2,
+    sequence: 1,
+  },
+  {
+    member_id: 3,
+    member_nickname: "탕수육이좋아요",
+    content: "제 돼지가 탕수육을 좋아해요",
+    create_at: "2022-09-07",
+    root: 1,
+    group: 1,
+    sequence: 2,
+  },
+];
--- frontend/src/pages/Recipe/recipe_detail_styles/index.js
+++ frontend/src/pages/Recipe/recipe_detail_styles/index.js
@@ -70,4 +70,134 @@ export const RecipeDetailIngredientElementDiv = styled.div`
 
 export const RecipeDetailContentWrapper = styled(
   RecipeDetailIngredientsWrapper
-)``;
+)`
+  display: flex;
+  flex-direction: columns;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const RecipeDetailContentElementDiv = styled(
+  RecipeDetailIngredientElementDiv
+)`
+  width: 94%;
+  padding: 5%;
+  font-weight: bolder;
+`;
+
+export const RecipeDetailLikeWrppaerDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 10%;
+  flex-direction: column;
+  width: 100%;
+  text-align: center;
+  align-items: center;
+`;
+
+export const RecipeDetailLikeBorderDiv = styled.div`
+  display: flex;
+  border-radius: 14px;
+  background-color: #d9d9d9;
+  padding-left: 3%;
+  padding-right: 3%;
+  padding-top: 2%;
+  padding-bottom: 2%;
+  width: 25%;
+  margin-bottom: 5%;
+  margin-top: 2%;
+`;
+
+export const RecipeDetailLikeCount = styled.div`
+  font-size: large;
+  font-weight: bolder;
+  text-align: center;
+  margin-left: auto;
+`;
+export const RecipeDetailCommentWraper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding-top: 5%;
+  border-top: 1px solid;
+`;
+
+export const RecipeDetailCommentElementWraper = styled(
+  RecipeDetailIngredientsWrapper
+)`
+  margin-top: 5%;
+  margin-bottom: 5%;
+  padding: 2%;
+`;
+
+export const RecipeDetailRecommentElementWrapper = styled(
+  RecipeDetailIngredientsWrapper
+)`
+  width: 85%;
+  margin-left: auto;
+  margin-right: 2.5%;
+  padding: 2%;
+  margin-bottom: 3%;
+  margin-top: 2%;
+`;
+
+export const RecipeDetailCommentElementBox = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const RecipeDetailCommentElementNameDiv = styled.div`
+  font-size: large;
+  font-weight: bolder;
+`;
+
+export const RecipeDetailCommentCreateWrapperDiv = styled.div`
+  display: flex;
+`;
+
+export const RecipeDetailCommentCreateAtDiv = styled.div`
+  font-size: small;
+  margin-left: auto;
+`;
+
+export const RecipeDetailCommentContentBox = styled.div`
+  font-weight: bold;
+`;
+
+export const RecipeDetailCommentContentInputWrapper = styled.div`
+  border-radius: 14px;
+  background-color: ${Palette.mainColor2};
+  width: 95%;
+  min-height: 8vh;
+  padding: 3%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const RecipeDetailCommentInput = styled.input`
+  width: 75%;
+  margin-right: auto;
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  background: none;
+  height: 4vh;
+`;
+
+export const RecipeDetailCommentButton = styled.button`
+  border-radius: 14px;
+  border: none;
+  background-color: ${Palette.mainColor5};
+  padding: 2%;
+  text-align: center;
+  font-weight: bolder;
+  :active {
+  }
+  cursor: pointer;
+`;
