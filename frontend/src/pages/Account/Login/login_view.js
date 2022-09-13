20bb6e58528acb2316e1e4df69556e19677dd93f
swoody <swoody1101@gmail.com>
2022-09-13 13:34:31 +0900

    feat: add login_view.js

--- frontend/src/pages/Account/Login/login_view.js
+++ frontend/src/pages/Account/Login/login_view.js
@@ -1,47 +1,65 @@
 import React, { useState } from "react";
 
+import {
+  Box,
+  Button,
+  createTheme,
+  Container,
+  FormControl,
+  Grid,
+  ThemeProvider,
+} from "@mui/material";
+
+import InputElement from "../components/input_element";
+
 const LoginView = () => {
+  const theme = createTheme();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const emailChangeHandler = (event) => {
-    setEmail(event.target.value);
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
   };
-
-  const passwordChangeHandler = (event) => {
-    setPassword(event.target.value);
-  };
-
-  // const submitHandler = (evnet) => {
-  //   event.preventDefault();
-  // };
 
   return (
-    <div class="loginregister">
-      <form>
-        <div>
-          <input
-            name="email"
-            type="email"
-            placeholder="이메일"
-            value={email}
-            onChange={emailChangeHandler}
-          />
-        </div>
-        <div>
-          <input
-            name="password"
-            type="password"
-            placeholder="비밀번호"
-            value={password}
-            onChange={passwordChangeHandler}
-          />
-        </div>
-        <div>
-          <button type="submit">로그인</button>
-        </div>
-      </form>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="sm">
+        <Box
+          component="form"
+          noValidate
+          onSubmit={formSubmitHandler}
+          sx={{ mt: 10 }}
+        >
+          <FormControl component="fieldset" variant="standard">
+            <Grid container spacing={2}>
+              <InputElement
+                label="이메일"
+                type="text"
+                value={email}
+                setValue={setEmail}
+              />
+              <InputElement
+                label="비밀번호"
+                type="password"
+                value={password}
+                setValue={setPassword}
+              />
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="outlined"
+              size="large"
+              sx={{ mt: 10 }}
+            >
+              로그인
+            </Button>
+            <Button>회원가입</Button>
+            <Button>비밀번호 찾기</Button>
+          </FormControl>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 };
 
