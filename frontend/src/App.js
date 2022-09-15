f10273217ae5318b283a9adeb2168faabebf2c2d
swoody <swoody1101@gmail.com>
2022-09-15 15:43:47 +0900

    refactor: remove import RecipeListView

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,11 @@ import SignUp from "./pages/Account/Signup/signup_view";
 import Login from "./pages/Account/Login/login_view";
 import NewPassword from "./pages/Account/Login/new_password_view";
 import MyPage from "./pages/Account/Mypage/mypage_view";
-import RecipeListView from "./pages/Recipe/List/recipe_list_page";
 
 function App() {
   return (
     <div className="App">
       <Global styles={Reset} />
-      <RecipeListView></RecipeListView>
       <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<SignUp />} />
