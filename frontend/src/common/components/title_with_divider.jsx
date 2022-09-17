89a64fd087a4ff0fd28e36feec402bf1115c07f1
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2022-09-17 23:46:17 +0900

    feat: adjust title that can be contained button icon
    타이틀 컴포넌트에 아이콘을 추가할 수 있도록 변경
    이를 기존 요소에 적용

--- frontend/src/common/components/title_with_divider.jsx
+++ frontend/src/common/components/title_with_divider.jsx
@@ -1,20 +1,58 @@
-import { Box, Divider, Typography } from "@mui/material";
+import {
+  Box,
+  Divider,
+  IconButton,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import { Palette } from "../styles/palette";
 
-function TitleWithDivider({ variant, title }) {
+function TitleWithDivider({
+  variant,
+  title,
+  onClick,
+  icon,
+  marginBottom,
+}) {
   return (
-    <Box>
-      <Typography
-        variant={variant}
-        fontWeight={"bold"}
-        color={Palette.black3}
-      >
-        {title}
-      </Typography>
-      <Divider sx={{ mb: 2 }} />
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        mb: `${marginBottom}rem`,
+      }}
+    >
+      <Box sx={{ display: "flex" }}>
+        <Typography
+          variant={variant}
+          fontWeight={"bold"}
+          color={Palette.black3}
+          flexGrow={1}
+        >
+          {title}
+        </Typography>
+
+        {onClick !== null ? (
+          <IconButton
+            onClick={onClick}
+            type="button"
+            sx={{ p: 0.5 }}
+          >
+            {icon}
+          </IconButton>
+        ) : null}
+      </Box>
+      <Divider />
     </Box>
   );
 }
+
+TitleWithDivider.defaultProps = {
+  variant: "h6",
+  title: "제목",
+  onClick: null,
+  icon: null,
+  marginBottom: 1,
+};
 
 export default TitleWithDivider;
