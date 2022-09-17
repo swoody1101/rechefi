00a02b1abf976596bc9bf7522f90de670e00ab23
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2022-09-18 00:54:09 +0900

    feat: change small design and fix props structure

--- frontend/src/pages/Recipe/Write/components/recipe_write_title_input.jsx
+++ frontend/src/pages/Recipe/Write/components/recipe_write_title_input.jsx
@@ -3,25 +3,29 @@ import React from "react";
 import { Palette } from "../../../../common/styles/palette";
 
 function RecipeWriteTitleInput({
-  value,
-  onChange,
+  title,
+  setValue,
   placeholder,
   validation,
   errorMessage,
 }) {
   const isError = () => {
     // dismiss value at first
-    if (value === null) return false;
+    if (title === null) return false;
     // validation ok
     if (validation()) return false;
     return true;
+  };
+
+  const onChange = (e) => {
+    setValue(e.target.value);
   };
 
   return (
     <Box sx={{ display: "flex", mt: 1, mx: 2 }}>
       <TextField
         variant="standard"
-        value={value}
+        value={title}
         placeholder={placeholder}
         onChange={onChange}
         error={isError()}
@@ -34,13 +38,14 @@ function RecipeWriteTitleInput({
           },
           "& ::placeholder": {
             fontWeight: "bold",
-            fontSize: "1.2rem",
+            fontSize: "1.4rem",
           },
         }}
         inputProps={{
+          maxLength: 21,
           style: {
             fontWeight: "bold",
-            fontSize: "1.2rem",
+            fontSize: "1.4rem",
             color: Palette.black3,
           },
         }}
