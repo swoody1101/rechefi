938e030b466be631e01b9eba7f83b2c59ba78dc1
swoody <swoody1101@gmail.com>
2022-09-19 17:42:18 +0900

    feat: 로그인 관련 수정

--- frontend/src/utils/http-commons.js
+++ frontend/src/utils/http-commons.js
@@ -1,16 +1,9 @@
 import axios from "axios";
 
-// const http = axios.create({
-//   baseURL: "http://localhost:8000",
-//   headers: {
-//     "content-type": "application/json; charset=UTF-8",
-//   },
-// });
-
 const http = axios.create({
   baseURL: "http://localhost:8000",
   headers: {
-    "content-type": "multipart/form-data",
+    "content-type": "application/json; charset=UTF-8",
   },
 });
 
