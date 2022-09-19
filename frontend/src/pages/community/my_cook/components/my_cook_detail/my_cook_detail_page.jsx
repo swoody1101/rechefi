fc48c42c5406eb4be7e820327cfee4188d519c0f
정진 <supermilktank8@gmail.com>
2022-09-19 16:13:28 +0900

    feat: 요리자랑 백엔드 연결

--- frontend/src/pages/community/my_cook/components/my_cook_detail/my_cook_detail_page.jsx
+++ frontend/src/pages/community/my_cook/components/my_cook_detail/my_cook_detail_page.jsx
@@ -8,6 +8,13 @@ import {
 } from "../my_cook_list/list_style";
 import { createPortal } from "react-dom";
 import RecipeDetailComments from "../../../../Recipe/recipe_detail/comments";
+import { useQuery } from "react-query";
+import http from "../../../../../utils/http-commons";
+
+const fetchMyCookDetail = async (param) => {
+  const res = await http.get(`/community/gallery/detail/1`);
+  return res.data;
+};
 
 export const MyCookDetail = ({ postId }) => {
   const [post, setPost] = useState({
@@ -17,7 +24,7 @@ export const MyCookDetail = ({ postId }) => {
     date: "",
     member_id: 0,
     member_nickname: "",
-    comment_count: 0,
+    comments: [],
     image_url: "",
   });
   useEffect(() => {
@@ -32,21 +39,50 @@ export const MyCookDetail = ({ postId }) => {
       window.scrollTo(0, parseInt(scrollY || "0", 10) * -1);
     };
   }, []);
-
-  useEffect(() => {
-    setPost({
-      id: 1,
-      title: "안녕하세요",
-      likes: 10,
-      date: "어머니가 해주신 짬뽕이에요",
-      member_id: 1,
-      member_nickname: "기타치는이현태",
-      comment_count: 5,
-      image_url: "/img/apples.jpg",
-      content: "아아아아아아아아아아아아아",
-    });
-  }, []);
-
+  const { isLoading, isError, data, error } = useQuery(
+    "myCookDetail",
+    fetchMyCookDetail,
+    {
+      onSuccess: (data) => {
+        console.log(data.data);
+        const temp = data.data;
+        setPost({
+          id: postId,
+          title: temp.title,
+          likes: temp.like_users,
+          date: temp.create_at,
+          member_id: temp.user_id,
+          member_nickname: temp.nickname,
+          comments: temp.comments,
+          image_url: temp.img_url,
+          views: temp.views,
+          content: temp.content,
+        });
+      },
+      onError: (e) => {
+        console.log(e.message);
+      },
+    }
+  );
+  // useEffect(() => {
+  //   setPost({
+  //     id: 1,
+  //     title: "안녕하세요",
+  //     likes: 10,
+  //     date: "어머니가 해주신 짬뽕이에요",
+  //     member_id: 1,
+  //     member_nickname: "기타치는이현태",
+  //     comment_count: 5,
+  //     image_url: "/img/apples.jpg",
+  //     content: "아아아아아아아아아아아아아",
+  //   });
+  // }, []);
+  if (isLoading) {
+    return <div>로딩중...</div>;
+  }
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
   return createPortal(
     <MyCookDetailWrapper>
       <MyCookDetailImageWrapper>
