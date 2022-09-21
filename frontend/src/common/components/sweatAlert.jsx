f26c2720b5999e9bcd8be6471f790f39543f1708
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2022-09-21 16:27:31 +0900

    feat: add success and confrim alert

--- frontend/src/common/components/sweatAlert.jsx
+++ frontend/src/common/components/sweatAlert.jsx
@@ -1,4 +1,5 @@
 import Swal from "sweetalert2";
+import { Palette } from "../styles/palette";
 
 export function Warn(title) {
   Swal.fire({
@@ -6,5 +7,28 @@ export function Warn(title) {
     title: title,
     timer: 1500,
     showConfirmButton: false,
+  });
+}
+
+export function Success(title) {
+  Swal.fire({
+    icon: "success",
+    title: title,
+    timer: 1500,
+    showConfirmButton: false,
+  });
+}
+
+export function Confirm(title, onConfirm, onCancel) {
+  Swal.fire({
+    title: title,
+    showCancelButton: true,
+    confirmButtonColor: Palette.mainColor4,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      if (onConfirm) onConfirm();
+    } else {
+      if (onCancel) onCancel();
+    }
   });
 }
