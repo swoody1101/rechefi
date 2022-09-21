1eb4ac3740c8088f2be9a1d930f12340c65f7790
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2022-09-21 16:28:03 +0900

    feat: add recipe confirm and cancle button

---
+++ frontend/src/pages/Recipe/Write/components/recipe_write_bottombar.jsx
@@ -0,0 +1,31 @@
+import { Box } from "@mui/material";
+import React from "react";
+import { Palette } from "../../../../common/styles/palette";
+import RecipeWriteBottombarButton from "./recipe_write_bottombar_btn";
+
+function RecipeWriteBottombar({ onConfirm, onCancel }) {
+  return (
+    <Box
+      sx={{
+        m: 2,
+        display: "flex",
+        justifyContent: "space-between",
+      }}
+    >
+      <RecipeWriteBottombarButton
+        onClick={onConfirm}
+        color={Palette.mainColor4}
+      >
+        완료
+      </RecipeWriteBottombarButton>
+      <RecipeWriteBottombarButton
+        onClick={onCancel}
+        color={Palette.gray3}
+      >
+        취소
+      </RecipeWriteBottombarButton>
+    </Box>
+  );
+}
+
+export default RecipeWriteBottombar;
