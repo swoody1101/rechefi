c89a11d7b7fdbc0a5b2a7927a1d124b25abe8b6a
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2022-09-23 17:04:29 +0900

    reactor: separate list hooks
    임시로 만들어 놓은것
    무한 스크롤 관련되서는 reactQuery로 대체 가능

---
+++ frontend/src/hooks/Recipe/list/useRecipes.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { Warn } from "../../../common/components/sweatAlert";
+import http from "../../../utils/http-commons";
+
+export function useRecipes() {
+  const [recipes, setRecipes] = useState([]);
+
+  const getRecipes = (recipe_id = 0) => {
+    http
+      .get(`/recipe/${recipe_id}`)
+      .then((response) => {
+        if (response.data.message === "success") {
+          setRecipes(recipes.concat(response.data.data));
+        }
+      })
+      .catch((error) => {
+        Warn(
+          `${error} 서버에 연결중 문제가 발생하였습니다`
+        );
+      });
+  };
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  return [recipes, getRecipes];
+}
