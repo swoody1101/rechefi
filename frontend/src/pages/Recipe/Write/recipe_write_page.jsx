fd7140c6a4dea3b90cf3ab049e0ce2c4f479e5b9
정진 <supermilktank8@gmail.com>
2022-09-26 17:11:54 +0900

    refactor: git pull front

--- frontend/src/pages/Recipe/Write/recipe_write_page.jsx
+++ frontend/src/pages/Recipe/Write/recipe_write_page.jsx
@@ -12,11 +12,7 @@ import RecipeWriteContentBlock from "./components/content/recipe_write_content_b
 import RecipeWriteContentImage from "./components/content/recipe_write_content_img";
 import RecipeWriteContentText from "./components/content/recipe_write_content_text";
 import RecipeWriteBottombar from "./components/bottombar/recipe_write_bottombar";
-import {
-  Confirm,
-  Success,
-  Warn,
-} from "../../../common/components/sweatAlert";
+import { Confirm, Success, Warn } from "../../../common/components/sweatAlert";
 import { useSelectedTag } from "../../../hooks/Recipe/tag/useSelectedTags";
 import { useTitle } from "../../../hooks/Recipe/write/useTitle";
 import { useContents } from "../../../hooks/Recipe/write/useContents";
@@ -29,8 +25,7 @@ function RecipeWritePage() {
   const [title, setTitle, titleValidation] = useTitle();
 
   // control tag information
-  const [selectedTags, addTag, deleteTag] =
-    useSelectedTag();
+  const [selectedTags, addTag, deleteTag] = useSelectedTag();
 
   // ingredients for recipe
   const [ingreds, setIngred] = useState([]);
@@ -63,6 +58,7 @@ function RecipeWritePage() {
   const writeComplete = () => {
     let tmp_contents = "";
     contents.forEach((item, index) => {
+      console.log(item);
       tmp_contents = tmp_contents.concat(
         convertToHTML(item.content.getCurrentContent())
       );
@@ -88,11 +84,7 @@ function RecipeWritePage() {
         navigate("/recipe", { replace: true });
       })
       .catch((error) => {
-        Warn(
-          error +
-            " : " +
-            "레시피 작성 중 문제가 발생하였습니다"
-        );
+        Warn(error + " : " + "레시피 작성 중 문제가 발생하였습니다");
       });
   };
 
@@ -121,10 +113,7 @@ function RecipeWritePage() {
       />
       {/* tags */}
       <RecipeWriteBox>
-        <RecipeListFilterTags
-          onTagAdded={addTag}
-          onTagDeleted={deleteTag}
-        />
+        <RecipeListFilterTags onTagAdded={addTag} onTagDeleted={deleteTag} />
       </RecipeWriteBox>
       {/* ingredients */}
       <RecipeWriteBox>
@@ -145,9 +134,7 @@ function RecipeWritePage() {
           >
             {/* image or text */}
             {block.type === "image" ? (
-              <RecipeWriteContentImage
-                link={block.content}
-              />
+              <RecipeWriteContentImage link={block.content} />
             ) : (
               <RecipeWriteContentText
                 index={index}
@@ -165,10 +152,7 @@ function RecipeWritePage() {
         onCancel={cancleWrite}
       />
 
-      <InputImage
-        setRef={imageInput}
-        onInput={addImageBlock}
-      />
+      <InputImage setRef={imageInput} onInput={addImageBlock} />
     </Container>
   );
 }
