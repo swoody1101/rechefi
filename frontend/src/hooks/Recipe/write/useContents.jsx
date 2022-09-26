d8e05c9cf1e11f485265683b541b163a5e8ab36e
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2022-09-26 13:11:13 +0900

    refactor: separate contents hooks and input component

---
+++ frontend/src/hooks/Recipe/write/useContents.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+export function useContents() {
+  const [contents, setContents] = useState([]);
+
+  const addTextBlock = () => {
+    setContents([
+      ...contents,
+      { type: "text", content: "" },
+    ]);
+  };
+
+  const addImageBlock = (res) => {
+    setContents([
+      ...contents,
+      { type: "image", content: res },
+    ]);
+  };
+
+  const deleteBlock = (index) => {
+    setContents(
+      contents.filter((ele, idx) => index !== idx)
+    );
+  };
+
+  // text edited update
+  const updateTextContent = (index, updated) => {
+    setContents(
+      contents.map((ele, idx) =>
+        idx === index
+          ? { ...ele, content: JSON.stringify(updated) }
+          : ele
+      )
+    );
+  };
+
+  // handle block position
+  const downBlockPos = (index) => {
+    if (index === 0) return;
+
+    setContents([
+      ...contents.slice(0, index - 1),
+      contents[index],
+      contents[index - 1],
+      ...contents.slice(index + 1),
+    ]);
+  };
+  const upBlockPos = (index) => {
+    if (index === contents.length - 1) return;
+
+    setContents([
+      ...contents.slice(0, index),
+      contents[index + 1],
+      contents[index],
+      ...contents.slice(index + 2),
+    ]);
+  };
+
+  return [
+    contents,
+    setContents,
+    addTextBlock,
+    addImageBlock,
+    deleteBlock,
+    updateTextContent,
+    downBlockPos,
+    upBlockPos,
+  ];
+}
