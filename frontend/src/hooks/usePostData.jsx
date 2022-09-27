52a6ff674112852412cf05ec4f2385bc3bf24160
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2022-09-27 23:58:53 +0900

    feat: add handling error status

--- frontend/src/hooks/usePostData.jsx
+++ frontend/src/hooks/usePostData.jsx
@@ -1,4 +1,5 @@
 import { useMutation, useQueryClient } from "react-query";
+import { Warn } from "../common/components/sweatAlert";
 import http from "../utils/http-commons";
 
 export default function usePostData(queryKey) {
@@ -11,6 +12,15 @@ export default function usePostData(queryKey) {
     {
       onSuccess: (data) => {
         queryClient.invalidateQueries(queryKey);
+      },
+      onError: (error) => {
+        if (error.response.status === 401) {
+          Warn("로그인이 필요합니다");
+        } else {
+          Warn(
+            `${error.code}\n 서버와 통신중 문제가 발생하였습니다`
+          );
+        }
       },
     }
   );
