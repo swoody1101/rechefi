af1ce9dd8154dcf3976dc35d82ee3e8098dfdb7d
장진세 <holy_killer@naver.com>
2022-09-28 23:09:35 +0900

    fix: hotfix get_ai_response

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,7 @@ from tortoise.contrib.fastapi import register_tortoise
 from app.config import TORTOISE_ORM, settings
 from app.routers import router
 
-app = FastAPI(title="new project", version="0.51")
+app = FastAPI(title="new project", version="0.53")
 
 app.add_middleware(
     CORSMiddleware,
