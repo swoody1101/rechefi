c8d73d9cc14f3e3a67edaa2b91ca56128f33bdc5
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2022-09-28 14:17:21 +0900

    feat: connect with REST in notice board

--- frontend/src/pages/community/FreeBoard/List/components/notice/free_board_list_notices_items.jsx
+++ frontend/src/pages/community/FreeBoard/List/components/notice/free_board_list_notices_items.jsx
@@ -1,47 +1,50 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import ErrorMessagePaper from "../../../../../../common/components/error_message_paper";
+import { useFetch } from "../../../../../../hooks/useFetch";
+import LoadingSpinner from "../../../../../Recipe/List/components/recipe_list_loading_spinner";
 import FreeBoardListItem from "../item/free_board_list_item";
 
 function FreeBoardListNoticeItems() {
-  const data = [
-    {
-      id: 1,
-      title: "핫한 핫소스 실화냐",
-      likes: 10,
-      date: "2020-10-20",
-      member_id: 1,
-      member_nickname: "나다",
-      comment_count: 3,
-    },
-    {
-      id: 2,
-      title: "핫한 핫소스 실화냐",
-      likes: 10,
-      date: "2020-10-20",
-      member_id: 1,
-      member_nickname: "나다",
-      comment_count: 3,
-    },
-    {
-      id: 3,
-      title: "핫한 핫소스 실화냐",
-      likes: 10,
-      date: "2020-10-20",
-      member_id: 1,
-      member_nickname: "나다",
-      comment_count: 3,
-    },
-  ];
+  const navigate = useNavigate();
+
+  // handle server data
+  const QUERY_KEY = "NOTICE_LIST";
+  const { isLoading, isError, data } = useFetch({
+    queryKey: QUERY_KEY,
+    param: 1,
+    uri: "/community/notice-board",
+  });
 
   return (
     <>
-      {data.map((item, index) => (
-        <FreeBoardListItem
-          key={item.id}
-          post={item}
-          isNotice={true}
-          isLast={data.length - 1 === index}
+      {isLoading ? (
+        <LoadingSpinner loading={true} />
+      ) : isError ? (
+        <ErrorMessagePaper
+          message={
+            "공지을 불러오는 중 문제가 발생하였습니다"
+          }
         />
-      ))}
+      ) : data.posts.length === 0 ? (
+        <ErrorMessagePaper
+          message={"작성된 공지가 없습니다"}
+        />
+      ) : (
+        data.posts.map((item, index) => (
+          <FreeBoardListItem
+            key={item.id}
+            post={item}
+            isNotice={true}
+            isLast={data.length - 1 === index}
+            onClick={() =>
+              navigate(
+                `/community/free-board/detail/${item.id}`
+              )
+            }
+          />
+        ))
+      )}
     </>
   );
 }
