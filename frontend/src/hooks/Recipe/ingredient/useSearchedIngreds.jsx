0c4826c92f29408c5bf4092a9a5d728a22ca1e6f
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2022-09-29 14:45:56 +0900

    feat: add Ingredient search in filter

--- frontend/src/hooks/Recipe/ingredient/useSearchedIngreds.jsx
+++ frontend/src/hooks/Recipe/ingredient/useSearchedIngreds.jsx
@@ -1,29 +1,21 @@
 import { useState } from "react";
+import { Warn } from "../../../common/components/sweatAlert";
+import http from "../../../utils/http-commons";
 
 export function useSearchedIngreds() {
   const [searchedIngred, setSearchedIngred] = useState([]);
 
-  // DEBUG
   const searchIngred = (keyword) => {
-    // TODO : replace with REST API
-    setSearchedIngred([
-      {
-        id: 1,
-        name: "양파양파양파양파양파양파양파",
-      },
-      {
-        id: 2,
-        name: "마파",
-      },
-      {
-        id: 3,
-        name: "대파",
-      },
-      {
-        id: 4,
-        name: "파",
-      },
-    ]);
+    http
+      .get(`/recipe/ingredient?name=${keyword}`)
+      .then((reponse) => {
+        if (reponse.data.message === "success") {
+          setSearchedIngred(reponse.data.data);
+        }
+      })
+      .catch((error) => {
+        Warn(`${error} 재료 검색 중 문제가 발생하였습니다`);
+      });
   };
 
   return [searchedIngred, searchIngred];
