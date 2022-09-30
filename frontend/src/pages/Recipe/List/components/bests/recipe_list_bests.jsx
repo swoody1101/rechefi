229414932fcdb07742fbc37f5e65de2152a72e3b
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2022-09-30 14:41:45 +0900

    feat: add navigate in best recipes

--- frontend/src/pages/Recipe/List/components/bests/recipe_list_bests.jsx
+++ frontend/src/pages/Recipe/List/components/bests/recipe_list_bests.jsx
@@ -3,14 +3,23 @@ import { Container } from "@mui/material";
 import Carousel from "react-material-ui-carousel";
 import BestRecipeCard from "./recipe_list_bests_card";
 import TitleWithDivider from "../../../../../common/components/title_with_divider";
+import { useNavigate } from "react-router-dom";
 
 function RecipeListBests({ bestRecipes }) {
+  const navigate = useNavigate();
+
   return (
     <Container>
       <TitleWithDivider textVariant="h5" title="베스트 레시피" />
       <Carousel animation="slide" interval="6000" sx={{ width: "100%" }}>
         {bestRecipes.map((item, i) => (
-          <BestRecipeCard bestRecipe={item} key={i} />
+          <BestRecipeCard
+            bestRecipe={item}
+            key={i}
+            onClick={() => {
+              navigate(`/recipe/postId=${item.id}`);
+            }}
+          />
         ))}
       </Carousel>
     </Container>
