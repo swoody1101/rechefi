df811a38ddb847b36973e8eb2eb12af5057cddca
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2022-09-30 10:54:36 +0900

    feat: add modified post load hook

--- frontend/src/hooks/FreeBoard/useModified.jsx
+++ frontend/src/hooks/FreeBoard/useModified.jsx
@@ -3,26 +3,28 @@ import { Warn } from "../../common/components/sweatAlert";
 import http from "../../utils/http-commons";
 
 function useModified(postId, isNotice) {
-  const [modified, setModified] = useState();
+  const [modified, setModified] = useState(null);
 
   // get modified post data from server
   useEffect(() => {
-    http
-      .get(
-        `/community/${
-          isNotice ? "notice-board" : "free-board"
-        }/detail/${postId}`
-      )
-      .then((response) => {
-        if (response.data.message === "success") {
-          setModified(response.data.data);
-        } else {
-          throw new Error(response.data.message);
-        }
-      })
-      .catch(() => {
-        Warn("수정할 글을 불러오는 중 문제가 발생하였습니다");
-      });
+    if (postId !== -1) {
+      http
+        .get(
+          `/community/${
+            isNotice ? "notice-board" : "free-board"
+          }/detail/${postId}`
+        )
+        .then((response) => {
+          if (response.data.message === "success") {
+            setModified(response.data.data);
+          } else {
+            throw new Error(response.data.message);
+          }
+        })
+        .catch(() => {
+          Warn("수정할 글을 불러오지 못했습니다");
+        });
+    }
   }, []);
 
   return [modified, setModified];
