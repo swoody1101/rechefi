8446aadf94efd7fb9a6a4e6c51a0c88e73bf6062
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2022-10-01 18:30:22 +0900

    feat: remove filter on recipe list and change filter design

--- frontend/src/pages/Recipe/List/components/recipe_list.jsx
+++ frontend/src/pages/Recipe/List/components/recipe_list.jsx
@@ -1,77 +1,18 @@
-import React, { useState } from "react";
-import { Box, Container } from "@mui/material";
+import React from "react";
+import { Box } from "@mui/material";
 import RecipeListItem from "./recipe_list_item";
-import RecipeListFilter from "./filter/recipe_list_filter_container";
-import RecipeListLoadingSpinner from "./recipe_list_loading_spinner";
 
-function RecipeList({ recipes, loading, onRecipeItemClicked }) {
-  const [filter, setFilter] = useState({
-    tags: [],
-    ingreds: [],
-  });
-
-  // recipes data render
-  const recipeItems = recipes.map((recipe) => {
-    // validate tags filter
-    let isTagFilterd = filter.tags.length === 0 ? true : false;
-    if (recipe.tags !== undefined) {
-      for (let i = 0; i < filter.tags.length; i++) {
-        for (let j = 0; j < recipe.tags.length; j++) {
-          if (filter.tags[i] === recipe.tags[j].id) {
-            isTagFilterd = true;
-          }
-        }
-      }
-    }
-    if (!isTagFilterd) return null;
-
-    // validate ingredient filter
-    let isIngredFilterd = filter.ingreds.length === 0 ? true : false;
-    if (recipe.ingredients !== undefined) {
-      for (let i = 0; i < filter.ingreds.length; i++) {
-        for (let j = 0; j < recipe.ingredients.length; j++) {
-          // include
-          if (
-            filter.ingreds[i].name === recipe.ingredients[j].name &&
-            filter.ingreds[i].include
-          ) {
-            isIngredFilterd = true;
-          }
-        }
-      }
-    }
-
-    // validate ingredient filter
-    if (recipe.ingredients !== undefined) {
-      for (let i = 0; i < filter.ingreds.length; i++) {
-        for (let j = 0; j < recipe.ingredients.length; j++) {
-          // exclude
-          if (
-            !filter.ingreds[i].include &&
-            filter.ingreds[i].name !== recipe.ingredients[j].name
-          ) {
-            isIngredFilterd = true;
-          }
-        }
-      }
-    }
-    if (!isIngredFilterd) return null;
-
-    return (
-      <RecipeListItem
-        key={recipe.id}
-        recipe={recipe}
-        onClick={() => onRecipeItemClicked(recipe.id, recipe.title)}
-      />
-    );
-  });
-
+function RecipeList({ recipes, onRecipeItemClicked }) {
   return (
-    <>
-      <RecipeListFilter onFilterApplied={setFilter} />
-      <Box>{recipeItems}</Box>
-      <RecipeListLoadingSpinner loading={loading} />
-    </>
+    <Box>
+      {recipes.map((recipe) => (
+        <RecipeListItem
+          key={recipe.id}
+          recipe={recipe}
+          onClick={() => onRecipeItemClicked(recipe.id, recipe.title)}
+        />
+      ))}
+    </Box>
   );
 }
 
