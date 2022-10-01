68f29f6793f05e7cb65c43d47387d689888d0909
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2022-10-01 21:23:10 +0900

    feat: replace sidebar with mui

--- frontend/src/common/components/sidebar/sidebar.jsx
+++ frontend/src/common/components/sidebar/sidebar.jsx
@@ -1,15 +1,23 @@
-import { Backdrop, SideBarWrapper } from "../../styles/sidebar_styles";
+import { SwipeableDrawer } from "@mui/material";
+import useWidthQuery from "../../../hooks/Main/useWidthQuery";
 import SidebarElementList from "./sidebar_list";
 
+const SideBar = ({ anchor, sidebarClose }) => {
+  const [sm, m, xl] = useWidthQuery();
+  const sidebarWidth = xl ? "28%" : m ? "36%" : sm ? "72%" : "72%";
 
-const SideBar = ({ sidebarClose }) => {
   return (
-    <div>
-      <SideBarWrapper>
-        <SidebarElementList sidebarClose={sidebarClose} />
-      </SideBarWrapper>
-      <Backdrop onClick={sidebarClose} />
-    </div>
+    <SwipeableDrawer
+      transitionDuration={500}
+      anchor={"left"}
+      open={anchor}
+      onClose={sidebarClose}
+      PaperProps={{
+        sx: { width: sidebarWidth },
+      }}
+    >
+      <SidebarElementList sidebarClose={sidebarClose} />
+    </SwipeableDrawer>
   );
 };
 
