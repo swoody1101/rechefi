bcf289659adb766a7488d845405fdd651ac0b672
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2022-10-04 14:47:50 +0900

    feat: add dynamic with container

---
+++ frontend/src/common/components/responsive_container.jsx
@@ -0,0 +1,13 @@
+import { Container } from "@mui/material";
+import useWidthQuery from "../../hooks/Main/useWidthQuery";
+
+function ReponsiveContainer({ children, style }) {
+  // window size check queries
+  const [sm, m, xl] = useWidthQuery();
+  // avartar size
+  const width = xl ? "50%" : m ? "75%" : sm ? "100%" : "100%";
+
+  return <Container sx={{ width: width, ...style }}>{children}</Container>;
+}
+
+export default ReponsiveContainer;
--- frontend/src/pages/Recipe/recipe_detail/recipe_detail.jsx
+++ frontend/src/pages/Recipe/recipe_detail/recipe_detail.jsx
@@ -24,32 +24,39 @@ import { useSelector } from "react-redux";
 import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
 import { useLike } from "../../../hooks/useLike";
 import { MyCookDetailListLoadingWrapper } from "../../community/my_cook/styles/list/list_style";
-import RecipeListLoadingSpinner from "../List/components/recipe_list_loading_spinner";
+import LoadingSpinner from "../List/components/recipe_list_loading_spinner";
 import AiArea from "./ai_area";
 import { Backdrop } from "../../../common/styles/sidebar_styles";
 import { aiReadingFormat } from "../../../store/module/AiReducer";
 import { useDispatch } from "react-redux";
+import { Container } from "@mui/material";
+import ReponsiveContainer from "../../../common/components/responsive_container";
 
 const RecipeDetail = () => {
   const dispatch = useDispatch();
   const [aiButton, setAiButton] = useState(false);
+
+  //
   const { detail } = useParams();
+
+  // get recipe data
   const { data, isLoading } = useFetchDetail({
     queryKey: "recipeDetail",
     articleId: detail,
     uri: "/recipe/detail/",
   });
+
   const userInfo = useSelector((store) => store.account);
+
   const toggleAI = () => {
     dispatch(aiReadingFormat());
-
     setAiButton((prev) => {
       return !prev;
     });
   };
+
   const [like, setLike] = useState(false);
   const { mutate } = useLike("recipeDetail");
-
   const likeHandler = () => {
     if (userInfo.auth) {
       mutate({ articleId: detail, uri: "/recipe/like/" });
@@ -59,6 +66,7 @@ const RecipeDetail = () => {
   useEffect(() => {
     dispatch(aiReadingFormat());
   }, []);
+
   useEffect(() => {
     if (userInfo.auth && data && data.data.like_users.length >= 0) {
       const like_users = data.data.like_users;
@@ -76,69 +84,70 @@ const RecipeDetail = () => {
   if (isLoading) {
     return (
       <MyCookDetailListLoadingWrapper>
-        <RecipeListLoadingSpinner loading={isLoading} />
+        <LoadingSpinner loading={isLoading} />
       </MyCookDetailListLoadingWrapper>
     );
   }
   return (
-    <RecipteDetailWrapperDiv>
-      <RecipeDetailAllContentWrapper>
-        {aiButton ? (
-          <div>
-            <AiArea content={data.data.recipe.content} toggleAI={toggleAI} />
-            <Backdrop />
-          </div>
-        ) : null}
-        <RecipeDetailTitleWrapperDiv>
-          <RecipedetailTitleArea
-            post={{
-              title: data.data.recipe.title,
-              member_nickname: data.data.user.nickname,
-              date: data.data.recipe.created_at,
-            }}
-          />
-        </RecipeDetailTitleWrapperDiv>
+    <ReponsiveContainer
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      {aiButton ? (
+        <div>
+          <AiArea content={data.data.recipe.content} toggleAI={toggleAI} />
+          <Backdrop />
+        </div>
+      ) : null}
+      <RecipeDetailTitleWrapperDiv>
+        <RecipedetailTitleArea
+          post={{
+            title: data.data.recipe.title,
+            member_nickname: data.data.user.nickname,
+            date: data.data.recipe.created_at,
+          }}
+        />
+      </RecipeDetailTitleWrapperDiv>
 
-        <RecipeDetailAIButtonWrapper>
-          <RecipeDetailAIButton onClick={toggleAI}>
-            레시피 읽어주기<SpatialTrackingIcon></SpatialTrackingIcon>
-          </RecipeDetailAIButton>
-        </RecipeDetailAIButtonWrapper>
+      <RecipeDetailAIButtonWrapper>
+        <RecipeDetailAIButton onClick={toggleAI}>
+          레시피 읽어주기<SpatialTrackingIcon></SpatialTrackingIcon>
+        </RecipeDetailAIButton>
+      </RecipeDetailAIButtonWrapper>
 
-        <RecipeDetailIngredinetsContentDiv>
-          <RecipeDetailIngredients ingredients={data.data.ingredients} />
-        </RecipeDetailIngredinetsContentDiv>
-        <RecipeDetailIngredinetsContentDiv>
-          <RecipeDetailContentWrapper>
-            <RecipeDetailContent content={data.data.recipe.content} />
-          </RecipeDetailContentWrapper>
-        </RecipeDetailIngredinetsContentDiv>
-        <RecipeDetailLikeWrppaerDiv onClick={likeHandler}>
-          <div>추천하기</div>
-          <RecipeDetailLikeBorderDiv>
-            {like ? (
-              <div>
-                <ThumbUpIcon />
-              </div>
-            ) : (
-              <div>
-                <ThumbUpOffAltIcon />
-              </div>
-            )}
-            <RecipeDetailLikeCount>
-              {data.data.like_users.length}
-            </RecipeDetailLikeCount>
-          </RecipeDetailLikeBorderDiv>
-        </RecipeDetailLikeWrppaerDiv>
-        <RecipeDetailIngredinetsContentDiv>
-          <Comments
-            uri={"/recipe/comment/"}
-            aiButton={aiButton}
-            queryKey="recipeComments"
-          />
-        </RecipeDetailIngredinetsContentDiv>
-      </RecipeDetailAllContentWrapper>
-    </RecipteDetailWrapperDiv>
+      <RecipeDetailIngredinetsContentDiv>
+        <RecipeDetailIngredients ingredients={data.data.ingredients} />
+      </RecipeDetailIngredinetsContentDiv>
+      <RecipeDetailIngredinetsContentDiv>
+        <RecipeDetailContentWrapper>
+          <RecipeDetailContent content={data.data.recipe.content} />
+        </RecipeDetailContentWrapper>
+      </RecipeDetailIngredinetsContentDiv>
+      <RecipeDetailLikeWrppaerDiv onClick={likeHandler}>
+        <div>추천하기</div>
+        <RecipeDetailLikeBorderDiv>
+          {like ? (
+            <div>
+              <ThumbUpIcon />
+            </div>
+          ) : (
+            <div>
+              <ThumbUpOffAltIcon />
+            </div>
+          )}
+          <RecipeDetailLikeCount>
+            {data.data.like_users.length}
+          </RecipeDetailLikeCount>
+        </RecipeDetailLikeBorderDiv>
+      </RecipeDetailLikeWrppaerDiv>
+
+      <RecipeDetailIngredinetsContentDiv>
+        <Comments
+          uri={"/recipe/comment/"}
+          aiButton={aiButton}
+          queryKey="recipeComments"
+        />
+      </RecipeDetailIngredinetsContentDiv>
+    </ReponsiveContainer>
   );
 };
 
