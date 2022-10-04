08f9dc32fa798e533b41f84bb745a7211b3551a4
정진 <supermilktank8@gmail.com>
2022-10-05 00:37:19 +0900

    refactor: 모듈 교체

--- frontend/src/pages/Recipe/recipe_detail/ai_listen_area.jsx
+++ frontend/src/pages/Recipe/recipe_detail/ai_listen_area.jsx
@@ -12,7 +12,6 @@ import {
 import AiListen from "./ai_listen";
 import { useEffect } from "react";
 import { Box, Button } from "@mui/material";
-import AiListenRecorder from "./ai_listen_recorder";
 
 const AiListenArea = ({ synth, toggleAI, recognition }) => {
   const aiListen = useSelector((store) => store.aiReducer.aiListen.nowListen);
@@ -28,7 +27,7 @@ const AiListenArea = ({ synth, toggleAI, recognition }) => {
     <div>
       <AiAreaListenWrapper>
         <Box sx={{ height: "100%" }}>
-          <AiListenRecorder
+          <AiListen
             synth={synth}
             toggleAI={toggleAI}
             recognition={recognition}
