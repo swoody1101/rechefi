5252e1df4b354ce83ee213caa7692f6fa37565e4
YeoUl <41969902+YeoUlFox@users.noreply.github.com>
2022-10-05 11:03:27 +0900

    feat: change write page file name and fix structure

--- frontend/src/pages/community/my_cook/components/write/my_cook_write_page.jsx
+++ frontend/src/pages/community/my_cook/components/write/my_cook_write_page.jsx
@@ -17,21 +17,26 @@ import { useNavigate } from "react-router-dom";
 import useAddMyCook from "../../../../../hooks/my_cook/useAddMyCook";
 import { Confirm, Success } from "../../../../../common/components/sweatAlert";
 import WriteButtonBar from "./write_buttom_bar";
+import ReponsiveContainer from "../../../../../common/components/responsive_container";
+import TitleWithDivider from "../../../../../common/components/title_with_divider";
+import { Box, Button, Paper, Typography } from "@mui/material";
+import ArticleIcon from "@mui/icons-material/Article";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
+import { Palette } from "../../../../../common/styles/palette";
+import MyCookWriteImageUploader from "./components/my_cook_write_image_uploader";
 
 const MyCookWriter = () => {
   const [searchModal, setSearchModal] = useState(false);
-  const [imageUploadUrl, setImageUploadUrl] = useState("");
+
+  // uplaoded image
+  const [image, setImage] = useState("");
+
   const [content, setContent] = useState("");
   const [referenceRecipe, setReferenceRecipe] = useState({});
 
   const navigate = useNavigate();
 
   const { mutate } = useAddMyCook("myCookPosts");
-
-  const uploadHandler = (keyword) => {
-    setImageUploadUrl(keyword);
-    console.log(keyword);
-  };
 
   const textHandler = (keyword) => {
     setContent(keyword);
@@ -43,7 +48,7 @@ const MyCookWriter = () => {
   };
 
   const vali = () => {
-    if (imageUploadUrl.length > 0 && content.length > 0) {
+    if (image.length > 0 && content.length > 0) {
       return false;
     }
     return true;
@@ -55,7 +60,7 @@ const MyCookWriter = () => {
     mutate(
       {
         uri: "/community/gallery",
-        sendData: { content, imageUploadUrl, recipe_id: referenceRecipe.id },
+        sendData: { content, image, recipe_id: referenceRecipe.id },
       },
       {
         onSuccess: () => {
@@ -72,7 +77,7 @@ const MyCookWriter = () => {
   };
 
   return (
-    <WriteWrapper>
+    <ReponsiveContainer style={{ display: "flex", flexDirection: "column" }}>
       {searchModal && (
         <div>
           <RecipeModal onRecipeItemClicked={onRecipeItemClicked} />
@@ -83,6 +88,36 @@ const MyCookWriter = () => {
           />
         </div>
       )}
+      <Box sx={{ display: "flex", justifyContent: "space-between", mb: 2 }}>
+        <Button
+          variant="outlined"
+          sx={{
+            p: 1,
+            borderColor: Palette.black2,
+            color: Palette.black2,
+            "&:focus, &:hover": {
+              color: Palette.white2,
+              backgroundColor: Palette.gray3,
+              borderColor: Palette.gray3,
+            },
+          }}
+        >
+          <AddPhotoAlternateIcon />
+          <Typography fontWeight={"bold"} sx={{ ml: 1 }}>
+            업로드
+          </Typography>
+        </Button>
+
+        <Button variant="outlined" sx={{ p: 1 }}>
+          <ArticleIcon />
+          <Typography fontWeight={"bold"} sx={{ ml: 1 }}>
+            레시피
+          </Typography>
+        </Button>
+      </Box>
+
+      <MyCookWriteImageUploader imageFile={image} setImageFile={setImage} />
+
       <RecipeListSearchWithResultDiv>
         <RecipeListSearchResultButton
           onClick={() => {
@@ -97,8 +132,8 @@ const MyCookWriter = () => {
           {referenceRecipe.title}
         </SelectReferenceRecipeDiv>
       </RecipeListSearchWithResultDiv>
+
       <WriteAreaWrapper>
-        <UploadImageArea uploadHandler={uploadHandler} />
         <WriteTextArea textHandler={textHandler} />
       </WriteAreaWrapper>
       <WriteButtonBar
@@ -106,7 +141,7 @@ const MyCookWriter = () => {
         onCancel={onCancel}
         onConfirm={onConfirm}
       />
-    </WriteWrapper>
+    </ReponsiveContainer>
   );
 };
 
