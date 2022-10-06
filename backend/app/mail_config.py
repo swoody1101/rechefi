0b1ff4ef204fcd2a89562f3fa87be1668fb4ffc2
장진세 <holy_killer@naver.com>
2022-10-07 02:33:11 +0900

    add: email template

--- backend/app/mail_config.py
+++ backend/app/mail_config.py
@@ -12,43 +12,43 @@ conf = ConnectionConfig(
     MAIL_TLS=True,
     MAIL_SSL=False,
     USE_CREDENTIALS=True,
-    VALIDATE_CERTS=True
+    VALIDATE_CERTS=True,
+    TEMPLATE_FOLDER='app/templates/'
 )
+
 
 # 가입 인증 메일
 async def signup_mail(background_tasks: BackgroundTasks, email: str, token: str):
     try:
         message = MessageSchema(
-            subject="회원가입 인증을 요청합니다.",
+            subject="[RECHEFI]회원가입 인증을 요청합니다.",
             recipients=[email],
             # 개발용 메시지
             # body=f'아래 링크를 클릭하여 인증을 완료해주세요.\nhttp://localhost:8000/members/{email}/{token}\n인증 링크의 유효시간은 5분입니다.',
-
             # 배포용 메시지
-            body=f'아래 링크를 클릭하여 인증을 완료해주세요.\nhttps://j7b303.p.ssafy.io/api/members/{email}/{token}\n인증 링크의 유효시간은 5분입니다.',
+            # body=f'아래 링크를 클릭하여 인증을 완료해주세요.\nhttps://j7b303.p.ssafy.io/api/members/{email}/{token}\n인증 링크의 유효시간은 5분입니다.',
+            template_body={'email': email, 'token': token}
             )
         fm = FastMail(conf)
-
-        background_tasks.add_task(fm.send_message,message)
-
+        background_tasks.add_task(fm.send_message, message, template_name='email_template.html')
         return True
-        
     except:
         return False
+
 
 # 임시 비밀번호 발급 메일
 async def password_mail(background_tasks: BackgroundTasks, email: str, password: str):
     try:
         message = MessageSchema(
-            subject="임시 비밀번호를 발급하였습니다.",
+            subject="[RECHEFI]임시 비밀번호를 발급하였습니다.",
             recipients=[email],
-            body=f'다음과 같이 회원님의 임시 비밀번호를 발급합니다.\n{password}\n 로그인 후 비밀번호를 꼭 변경해주세요.',
+            # body=f'다음과 같이 회원님의 임시 비밀번호를 발급합니다.\n{password}\n 로그인 후 비밀번호를 꼭 변경해주세요.',
+            template_body={'email': email, 'password': password}
             )
         fm = FastMail(conf)
 
-        background_tasks.add_task(fm.send_message,message)
+        background_tasks.add_task(fm.send_message, message, template_name='email_password.html')
 
         return True
-    
     except:
         return False
